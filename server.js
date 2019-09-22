f9d33ed36b8094039015acf9ed2dbbf8ae8b9763
Lizaosan <lizaosan@gmail.com>
2019-09-22 13:42:50 +0800

    Deploy on Heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server).sockets;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+    console.log('a user connected');
+
+    socket.on('rotate', (angle) => {
+        // console.log(angle);
+        io.emit("getRotate", angle)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`Server running on port ${port}`))
